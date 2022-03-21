d668eeb7f2d3951ba8db93e55536f95c286efac1
Luis Galindo <egalindo54@uabc.edu.mx>
2022-03-20 20:52:21 -0700

    generador de tablas de verdad

--- python/logico.py
+++ python/logico.py
@@ -1,33 +1,5 @@
 
-variables = "a,b,c"
-funcion = "-b * (-a + c)"
-  
-class Logic:
-    def __init__(self, estatus: int):
-        self.estatus = bool(estatus)
-
-    def __add__(self, x):
-        return Logic(self.estatus or x.estatus)
-
-    def __sub__(self, x):
-        return Logic( not(self.estatus) and x.estatus )
-
-    def __neg__(self):
-        return Logic( not(self.estatus) )
-
-    def __mul__(self, x):
-        return Logic( self.estatus and x.estatus )
-
-    def __str__(self):
-        return str(int(self.estatus))
-        
-        
-def exp(elem,var,fun):
-    keys = var.split(",")
-    values = [ Logic(int(x)) for x in elem ]
-    dic = dict(zip(keys, values))
-    return eval("str("+fun+")",dic)
-
+# Warning: Codigo horrible
 
 def bin(number,digits):
     # mejor formato de binario
@@ -42,6 +14,34 @@ def binary(n):
     # genera una lista con los numeros binarios de 'n' bits
     return [bin(i,n) for i in range(0,1<<n)]
 
-def geval( n, f ):
-    for e in f(n):
-        print(e,exp(e,variables,funcion))
+def generate_values(variables, value):
+    # divide el strig en cantidad de variables
+    keys = variables.split(",")
+    # mismas claves pero con una 'n' al inicio
+    neg_keys = [ 'n' + x for x in keys ]
+    # extrae el valor individual de cada bit
+    values = [ bool(int(x)) for x in value ]
+    # valores negados
+    neg_values = [ not x for x in values ]
+    # retorna un diccionario
+    return dict(zip(keys+neg_keys+["n"],values+neg_values+[lambda x: not x]))
+
+def eval_function(function, values):
+    return eval( "int(bool(" + function.replace("-","n") + "))", values )
+
+def tabla_verdad(variables, function, orden):
+    for v in orden(len(variables.split(","))):
+        print(v, eval_function(function, generate_values(variables,v)))
+
+print("")
+print("1. gray / 2.binario")
+print("")
+ordenado = int(input(" Ordenado: "))
+variables =    input("Variables? ")
+funcion =      input("  Funcion? ")
+print("")
+
+if(ordenado==1):
+    tabla_verdad(variables, funcion, gray)
+else:
+    tabla_verdad(variables, funcion, binary)
